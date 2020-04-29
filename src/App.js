7fd04ab10c51046bdcb684ecb076944cbd5394b4
MaBowen <han947393839@qq.com>
2020-04-29 23:13:43 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Count from './components/Count/Count'
 export default class App extends Component {
     render() {
         return (
             <div>
-                app
+                <Count/>
             </div>
         )
     }
